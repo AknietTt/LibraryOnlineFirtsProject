ac4ba3a94ab7028dc59c915cc490f488442c48d6
Akniet Nurmuhanov <akniet28022004@gmail.com>
2023-01-22 18:24:24 +0600

    My first project

---
+++ src/main/java/Library/Project/repository/BooksRepository.java
@@ -0,0 +1,11 @@
+package Library.Project.repository;
+
+import Library.Project.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
+}
--- src/main/java/Library/Project/util/PersonValidator.java
+++ src/main/java/Library/Project/util/PersonValidator.java
@@ -2,6 +2,7 @@ package Library.Project.util;
 
 import Library.Project.DAO.PersonDAO;
 import Library.Project.models.Person;
+import Library.Project.service.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -10,11 +11,11 @@ import org.springframework.validation.Validator;
 @Component
 public class PersonValidator implements Validator {
 
-    private final PersonDAO personDAO;
+    private final PeopleService peopleService;
 
     @Autowired
-    public PersonValidator(PersonDAO personDAO) {
-        this.personDAO = personDAO;
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
     @Override
@@ -26,7 +27,7 @@ public class PersonValidator implements Validator {
     public void validate(Object o, Errors errors) {
         Person person = (Person) o;
 
-        if (personDAO.getPersonByFullName(person.getFullName()).isPresent())
+        if (peopleService.getPersonByFullName(person.getFullName()).isPresent())
             errors.rejectValue("fullName", "", "Человек с таким ФИО уже существует");
     }
 }
